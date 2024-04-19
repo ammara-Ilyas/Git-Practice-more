0467d26960762675ab9db4629a4e20ec2f933c36
ammara-Ilyas <ismarailyas545@gmail.com>
2024-04-19 12:28:27 +0500

    More Text files are added

---
+++ index.js
@@ -0,0 +1,75 @@
+// async function foo() {
+//   return "hello";
+// }
+// console.log(a);
+// console.log(b);
+// var a = (b = 5);
+// console.log(1 < 2 < 3);
+let str1 = "Java";
+
+let str2 = "Script";
+console.log("cookies");
+let result = str1 + str2;
+result = str1.concat(str2);
+
+console.log("Concatenated String: " + result);
+// console.log(3 > 2 > 1);
+// let max = -1;
+// let sec = -1;
+// let res = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       sec = max;
+//       max = arr[i];
+//     } else if (arr[i] < sec && arr[i] != max) {
+//       arr[i] = sec;
+//     }
+
+//   }
+//   return sec;
+// };
+// let c = res([1, 2, 3, 4, 5, 2, 4, 8, 6, 7, 2, 0]);
+// console.log(c);
+// const a = 5 + "5" - 5;
+// console.log(a);
+// let obj = {
+//   id: 2,
+//   a: undefined,
+//   b: null,
+//   d: "hello",
+// };
+// let c = Object.entries(obj);
+// console.log(c);
+// let d = c.filter(([str, val]) => {
+//   return val != null;
+// });
+// console.log(d);
+// let za = [1, 2, 3, 4];
+// console.log(Math.min(za));
+// let result = foo()
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+// console.log(result);
+// console.log([1, 2] == [1, 2]);
+const str = "hi i am good girl";
+// let arr = str.split(" ");
+// console.log(arr);
+// let newArr = arr.map((val) => {
+//   return val[0].toLocaleUpperCase() + val.slice(1);
+// });
+// console.log(newArr);
+// console.log(str);
+// let todo = document.getElementById("todo");
+// console.log(todo);
+// let input = document.getElementById("input");
+// let btn = document.getElementById("btn");
+// let arr = [];
+
+// btn.addEventListener("click", () => {
+//   arr.push(input);
+//   console.log(input.value);
+// });
+// let tod = arr.map((val) => {
+//   return val;
+// });
+// todo.innerHTML = tod;
